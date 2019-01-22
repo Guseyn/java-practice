b182ad7456e3614ea535ffc18d4a2625266c1612
gismayilov <guseynism@gmail.com>
2019-01-22 15:30:36 +0300

    Switch

---
+++ md/Switch.java
@@ -0,0 +1,19 @@
+package md;
+
+public class Switch {
+
+    public static void main(String[] main) {
+        int y = 1;
+        switch (y) {
+            default:
+                //break;
+            case 1:
+                System.out.println('2');
+                //break;
+            case 0:
+            case 2:
+                System.out.println('1');
+        }
+    }
+
+}
