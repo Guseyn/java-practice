2d38c8a53d940ba023a6987af6b014ee80cf6990
gismayilov <guseynism@gmail.com>
2019-01-23 16:00:14 +0300

    #11

---
+++ md/Primitives.java
@@ -0,0 +1,39 @@
+package md;
+
+public class Primitives {
+
+    public static void main(String[] main) {
+        short s = 1;
+        byte b = 2;
+        int i = 3;
+        long l = 4;
+        float f = 5.0f;
+        double d = 6.0;
+        int r1 = s + b + i;
+        float r2 = i + f;
+        int r3 = (int)f;
+        int r4 = (int)d;
+        float r5 = f + (float)d;
+        double d2 = (double)1.0f;
+        double d3 = 1.0f;
+        int r6 = s;
+        int r7 = b;
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+        System.out.println(r5);
+        System.out.println(r5);
+        System.out.println(d2);
+        System.out.println(d3);
+        System.out.println(r6);
+        System.out.println(r7);
+    }
+
+}
